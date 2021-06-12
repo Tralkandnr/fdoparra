d73a8e2cc8665c67cfbafef674d66f59b8385dfc
Tralkandnr <tralkan.dnr@gmail.com>
2021-06-12 19:42:01 -0400

    version1

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ul class="nav">
+          <li><a><img src="http://placekitten.com/50/50" /></a></li>
+          <li><a>Inicio</a></li>
+          <li><a>Acerca de</a></li>
+          <li><a>Galeria</a></li>
+          <li><a>Mis trabajos</a></li>
+        </ul>
+      </header>
+      <body>
+        <div>
+          Hola xd
+        </div>
+      </body>
+
+    </div>
+  );
+}
+export default App;
