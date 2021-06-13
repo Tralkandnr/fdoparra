c085072a17cf65743b30c9d0886d1813ee30935b
Tralkandnr <tralkan.dnr@gmail.com>
2021-06-13 00:01:34 -0400

    version 0.1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import './App.css';
+import React from 'react';
+import Navbar from './components/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <ul class="nav">
-          <li><a><img src="http://placekitten.com/50/50" /></a></li>
-          <li><a>Inicio</a></li>
-          <li><a>Acerca de</a></li>
-          <li><a>Galeria</a></li>
-          <li><a>Mis trabajos</a></li>
-        </ul>
-      </header>
-      <body>
-        <div>
-          Hola xd
-        </div>
-      </body>
-
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/NavBar/MenuItems.js
@@ -0,0 +1,23 @@
+const MenuItems = [
+    {
+        title: 'Inicio',
+        url:'#',
+        cName:'nav-links'
+    },
+    {
+        title: 'Acerca de',
+        url:'#',
+        cName:'nav-links'
+    },
+    {
+        title: 'Galería',
+        url:'#',
+        cName:'nav-links'
+    },
+    {
+        title: 'Mis trabajos',
+        url:'#',
+        cName:'nav-links'
+    }
+
+]
