9f8cdec4c942c03b9501c3edcbebb61f6b2a64cd
Tralkandnr <tralkan.dnr@gmail.com>
2021-06-15 23:23:00 -0400

    cambios leves a dieseño

---
+++ src/components/NavBar/Iconos.js
@@ -0,0 +1,16 @@
+export const Iconos = [
+
+    {
+        url:"#",
+        cName:"fab fa-facebook-square"
+    },
+    {
+        url:"#",
+        cName:"fab fa-instagram"
+    },
+    {
+        
+        url:"#",
+        cName:"fab fa-youtube"
+    }
+]
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,7 +2,9 @@ import React,  { Component } from 'react';
 import  { MenuItems } from "./MenuItems.js";
 import { MenuItems2 } from './MenuItems2.js';
 import './Navbar.css';
-import logo from '../../assets/img/logo.png'
+import logo from '../../assets/img/logo.png';
+import {Iconos} from './Iconos';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
 class Navbar extends Component {
@@ -32,6 +34,13 @@ class Navbar extends Component {
                 <a className={item.cName} href={item.url}>
                   {item.title}
                 </a>
+              </li>
+            );
+          })}
+          {Iconos.map((item, index)=>{
+            return (
+              <li key={index}>
+                <i class={item.cName} href={item.url}></i>
               </li>
             );
           })}
