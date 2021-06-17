bc7f5b7e92f5f5fb713c266bb15b8317e7af0153
Tralkandnr <tralkan.dnr@gmail.com>
2021-06-16 22:53:55 -0400

    agregado el toggle menu y su responsividad(ponerle mas style)

---
+++ src/components/NavBar/ContenigoToggle.js
@@ -0,0 +1,33 @@
+export const ContenidoToggle= [
+    {
+        title: 'Inicio',
+        url:'#',
+        cName:'toggle-links'
+    },
+    {
+        title: 'Acerca de',
+        url:'#',
+        cName:'toggle-links'
+    },
+    {
+        title: 'Galería',
+        url:'#',
+        cName:'toggle-links'
+    },
+    {
+        title: 'Contacto',
+        url:'#',
+        cName:'toggle-links'
+    },
+    {
+        title: 'Productos',
+        url:'#',
+        cName:'toggle-links'
+    },
+    {
+        title: 'Mis trabajos',
+        url:'#',
+        cName:'toggle-links'
+    }
+
+]
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,20 +4,30 @@ import { MenuItems2 } from './MenuItems2.js';
 import './Navbar.css';
 import logo from '../../assets/img/logo.png';
 import {Iconos} from './Iconos';
+import { ContenidoToggle } from './ContenigoToggle.js';
 
 class Navbar extends Component {
   state = {clicked: false}
   hacerClick = () => {
-    this.setState({clicked: !this.state.clicked})
+    this.setState({clicked:!this.state.clicked})
   }
-
   render() {
     return (
       <nav className="NavBarItems">
         <div className="menu-icon" onClick={this.hacerClick}>
-        <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+            <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
         </div>
-            
+        <div className={this.state.clicked ? 'toggle-desplegado':'toggle-oculto'}>
+          <ul className="toggle-menu">
+            {ContenidoToggle.map((item, index) => {
+            return(
+              <li key={index} className="toggle-li">
+                <a className={item.cName} href={item.url}>{item.title}</a>
+              </li>
+            );
+          })}
+          </ul>
+        </div>    
          <ul className="menu1">
           {MenuItems2.map((item, index) => {
             return (
